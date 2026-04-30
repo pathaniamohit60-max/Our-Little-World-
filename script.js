816d9bd5309e7956df7a5d8a24ab4d86620acf84
pathaniamohit60-max <pathaniamohit60@gmail.com>
2026-04-30 23:04:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+const startDate = new Date("2024-12-04");
+
+function updateDaysTogether() {
+
+    const today = new Date();
+
+    const difference =
+        today.getTime() - startDate.getTime();
+
+    const days = Math.floor(
+        difference / (1000 * 60 * 60 * 24)
+    );
+
+    document.getElementById("days").innerText = days;
+}
+
+updateDaysTogether();
+
+
+
+
+const reasons = [
+
+    "Your smile makes my worst days feel lighter.",
+
+    "You make ordinary moments feel magical.",
+
+    "Your voice feels like home.",
+
+    "You are the prettiest part of my life.",
+
+    "I love how you care about little things.",
+
+    "You make my world softer and happier."
+
+];
+
+
+
+function newReason() {
+
+    const randomReason = reasons[
+        Math.floor(Math.random() * reasons.length)
+    ];
+
+    document.getElementById("reasonText").innerText =
+        randomReason;
+}
+
+
+
+function secretMessage() {
+
+    alert(
+        "Thank you for existing. You make my life beautiful ❤️"
+    );
+
+}
